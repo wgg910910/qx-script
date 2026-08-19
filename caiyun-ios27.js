276efff3df66d0fba16d6192ded1376bce20531a
wgg910910 <48898502+wgg910910@users.noreply.github.com>
2026-08-19 13:05:57 +0800

    Update caiyun-ios27.js

--- caiyun-ios27.js
+++ caiyun-ios27.js
@@ -1,48 +1,9 @@
-// caiyun-ios27.js
-// Quantumult X + Apple WeatherKit v2
+console.log("WeatherKit script loaded");
 
-const url = $request.url || "";
-
-const reg = /\/api\/v2\/weather\/[^/]+\/(-?\d+(?:\.\d+)?)\/(-?\d+(?:\.\d+)?)/;
-
-const m = url.match(reg);
-
-if (m) {
-
-  let lat = m[1];
-  let lon = m[2];
-
-  $prefs.setValueForKey(lat, "caiyun_latitude");
-  $prefs.setValueForKey(lon, "caiyun_longitude");
-
-  $prefs.setValueForKey(
-    JSON.stringify({
-      latitude: lat,
-      longitude: lon
-    }),
-    "location"
-  );
-
-
-  $notify(
-    "彩云天气",
-    "定位成功",
-    `${lat}, ${lon}`
-  );
-
-  console.log(
-    "WeatherKit:",
-    lat,
-    lon
-  );
-
-} else {
-
-  console.log(
-    "没有匹配到:",
-    url
-  );
-
-}
+$notify(
+  "测试",
+  "脚本已执行",
+  "QX调用成功"
+);
 
 $done({});
